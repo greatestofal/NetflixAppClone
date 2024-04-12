1f724c25613aafd9643def0c68fbb39602dddfe5
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-12 21:29:51 +0100

    commit 2

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -13,14 +13,17 @@ const Row = ({ title, fetchURL }) => {
       <h2 className="text-white font-bold md:text-xl p-4"> {title}</h2>
       <div className="relative flex items-center">
         {/* Slider Div (Mapping through) */}
-        <div id={"slide"}>
+        <div id={"slider"}>
           {movies.map((item, id) => (
             <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
               <img
+                className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                 alt={item?.title}
               />
-              <div> </div>
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p> {item?.title}</p>
+              </div>
             </div>
           ))}
         </div>
