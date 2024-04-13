7f734e3774ecff0a398127a0b51534ef3d3f8627
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-13 23:10:06 +0100

    Row commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
       setMovies(response.data.results);
@@ -13,18 +15,12 @@ const Row = ({ title, fetchURL }) => {
       <h2 className="text-white font-bold md:text-xl p-4"> {title}</h2>
       <div className="relative flex items-center">
         {/* Slider Div (Mapping through) */}
-        <div id={"slider"}>
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative "
+        >
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p> {item?.title}</p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
